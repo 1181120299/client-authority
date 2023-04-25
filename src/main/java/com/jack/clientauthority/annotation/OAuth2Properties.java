c6ea28dd208ff94bb0d10a391dbff3442dac7a11
Jack <1181120299@qq.com>
2023-04-25 21:58:30 +0800

    refactor：jack.oauth2配置项增加非空以及后缀校验

--- src/main/java/com/jack/clientauthority/annotation/OAuth2Properties.java
+++ src/main/java/com/jack/clientauthority/annotation/OAuth2Properties.java
@@ -1,11 +1,13 @@
 package com.jack.clientauthority.annotation;
 
 import lombok.Data;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.Assert;
 
 @Data
 @ConfigurationProperties(prefix = "jack.oauth2")
-public class OAuth2Properties {
+public class OAuth2Properties implements InitializingBean {
 
     /**
      * 授权服务的地址，例如：http://ip:port/contextPath
@@ -37,4 +39,20 @@ public class OAuth2Properties {
      * 当前应用部署的服务器ip，需要和授权服务器上注册的OAuth client回调地址中的ip保持一致
      */
     private String clientIp;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Assert.hasText(authServerUri, "AuthServerUri must not be empty.");
+        Assert.hasText(resourceServerUri, "ResourceServerUri must not be empty.");
+        Assert.hasText(clientId, "ClientId must not be empty.");
+        Assert.hasText(clientSecret, "ClientSecret must not be empty.");
+
+        if (authServerUri.endsWith("/")) {
+            throw new IllegalStateException("AuthServerUri must not end with /");
+        }
+
+        if (resourceServerUri.endsWith("/")) {
+            throw new IllegalStateException("ResourceServerUri must not end with /");
+        }
+    }
 }
